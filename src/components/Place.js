dbd78beaab8e30705e0383d3b8d5dd02dd98c5ef
Haris <mhaaris0815@gmail.com>
2021-10-06 23:36:24 +0530

    added styling in components

--- src/components/Place.js
+++ src/components/Place.js
@@ -3,7 +3,7 @@ import React from "react";
 const Place = ({ city }) => {
   return (
     <div>
-      <h1>We all live in {city}</h1>
+      <h1 style={{ color: "red" }}>We all live in {city}</h1>
     </div>
   );
 };
